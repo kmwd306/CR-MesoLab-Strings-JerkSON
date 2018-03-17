6b574a3faec907a29e6a110c779d2b01708f43fd
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-4.local>
2018-03-17 19:57:44 -0400

    started

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,8 +2,16 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TreeMap;
 
 public class ItemParser {
+    //to organize my Key/Value pairs
+    private TreeMap<String, ArrayList<Item>> groceryMap;
+
+    public ItemParser(){
+        //initializing the TreeMap
+        groceryMap = new TreeMap<String, ArrayList<Item>>();
+    }
 
 
     public ArrayList<String> parseRawDataIntoStringArray(String rawData){
@@ -25,6 +33,11 @@ public class ItemParser {
     private ArrayList<String> splitStringWithRegexPattern(String stringPattern, String inputString){
         return new ArrayList<String>(Arrays.asList(inputString.split(stringPattern)));
     }
+
+
+
+
+
 
 
 
